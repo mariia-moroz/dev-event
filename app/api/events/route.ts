b1742ac0a731aae0f8022db967832e9bcb465549
maria-moroz <mma.moroz.mariia@gmail.com>
2026-04-19 17:42:53 +0100

    Added image uploader

--- app/api/events/route.ts
+++ app/api/events/route.ts
@@ -1,14 +1,13 @@
 import connectDB from "@/lib/mongodb";
 import { NextRequest, NextResponse } from "next/server";
 import Event from "@/database/event.model";
+import { v2 as cloudinary } from "cloudinary";
 
 export async function POST(req: NextRequest) {
   try {
     await connectDB();
 
     const formData = await req.formData();
-
-    console.log(formData);
 
     let event;
 
@@ -23,6 +22,39 @@ export async function POST(req: NextRequest) {
         { status: 400 },
       );
     }
+
+    // upload image
+
+    const file = formData.get("image") as File;
+
+    if (!file) {
+      return NextResponse.json(
+        {
+          message: "Image file is required",
+        },
+        { status: 400 },
+      );
+    }
+
+    const arrayBuffer = await file.arrayBuffer();
+
+    const buffer = Buffer.from(arrayBuffer);
+
+    const uploadResult = await new Promise((resolve, reject) => {
+      cloudinary.uploader
+        .upload_stream(
+          { resource_type: "image", folder: "DevEvent" },
+          (error, result) => {
+            if (error) {
+              return reject(error);
+            }
+            return resolve(result);
+          },
+        )
+        .end(buffer);
+    });
+
+    event.image = (uploadResult as { secure_url: string }).secure_url;
 
     const createdEvent = await Event.create(event);
 
